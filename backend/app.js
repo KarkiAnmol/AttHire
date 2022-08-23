c391d3f7cee3715b8cd813706bf0e887de52eb5d
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2022-08-23 21:22:50 +0545

    creating server using express and dotenv module to load environment variables

---
+++ backend/app.js
@@ -0,0 +1,4 @@
+const express = require("express"); //importing express module
+const app =express(); //calling the express() function which puts a  new express application inside the 'app' variable
+module.exports =app; //telling Node.js to give this file's access to other files
+
