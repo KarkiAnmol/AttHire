d1b1eefcff1e95794f29c59b94473d5cd1654bbf
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2022-09-09 19:27:36 +0545

    creating clothes schema and defining the attributes of clothe

---
+++ backend/models/clotheModel.js
@@ -0,0 +1,67 @@
+const mongoose= require('mongoose')
+const clotheSchema= mongoose.Schema({
+name:{
+    type:String,
+    required:[true,"You have to enter clothe name"]
+},
+description:{
+    type:String,
+    required:[true,"Please enter clothe description"]
+  
+},
+price:{
+    type:Number,
+    required:[true,"Please enter price of clothe"],
+    maxLength:[7,"price cannot exceed 7 figures"]
+},
+rating:{
+    type:Number,
+    default:0   
+},
+images:[{
+    public_id:{
+        type:String,
+        required:true
+    },
+
+    url:{
+        type:String,
+        required:true
+
+    }
+}],
+category:{
+    type:String,
+    required:[true,"Please enter clothe's category"]
+},
+stock:{
+    type:Number,
+    required:[true,"Please enter number of stocks"],
+    maxLength:[5,"Stock cannot exceed 5 figures"],
+    default:1
+},
+numberOfReviews:{
+    type:Number,
+    default:0
+},
+reviews:[{
+    name:{
+        type:String,
+        required:true
+    },
+    rating:{
+        type:Number,
+        required:true
+    },
+    comment:{
+        type:String,
+        required:true
+    }
+}],
+createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+mongoose.exports= mongoose.model("Clothe",clotheSchema);//mongoose.model compiles the clotheSchema
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ const app = require('./app');
 
 //dotend is a zero dependency module that loads environment variables from the .env file into process.env
 const dotenv=require("dotenv");
+const connectDataBase=require('./config/database')
+
+//set config
 dotenv.config({path:"backend/config/config.env"}); 
+
+//Connecting database
+connectDataBase();
 
 app.listen(process.env.PORT,()=>{ //creating a server
     // if(err) console.log("error in server sertup"); 
