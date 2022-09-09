9daa21947e43e0333165ef1718afc5b24d8df2d0
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2022-09-09 10:28:02 +0545

    routes were not working due to missing '()' in behind express.json

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 
 //dotend is a zero dependency module that loads environment variables from the .env file into process.env
-require("dotenv").config({path:"backend/config/config.env"}); 
+const dotenv=require("dotenv");
+dotenv.config({path:"backend/config/config.env"}); 
 
 app.listen(process.env.PORT,()=>{ //creating a server
-    // if(err) console.log("error in server sertup");
+    // if(err) console.log("error in server sertup"); 
     // else console.log(`Server is listening on PORT : ${process.env.PORT}`);
-    console.log(`Server is listening on PORT : ${process.env.PORT}`);
+    console.log(`Server is listening on PORT : ${process.env.PORT}`)
 })
