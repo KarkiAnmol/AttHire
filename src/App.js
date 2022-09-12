4dbbeaf4db85b27583df900261f60960f95c8369
Aakriti Banjara <banjarabinda@gmail.com>
2022-09-12 21:19:27 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import img_logo from "./images/Logo.png";
+import {Button ,Form , FormGroup ,Label , Input} from 'reactstrap';
+import {FacebookLoginButton } from 'react-social-login-buttons';
+import {GoogleLoginButton} from 'react-social-login-buttons';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="Login_form">
+      <div className="text-center">
+      <h1><span className='font-weight-bold'>ATTHIRE</span></h1>
+      <img src ={img_logo} height={60}/></div>
+       <p className='text-center'>Dress To Express</p>
+       <FormGroup>
+         <Label>Email</Label><br></br>
+         <Input type ="Email" placeholder='Email'/>
+         </FormGroup>
+         <FormGroup>
+         <Label>Password</Label><br></br>
+         <Input type ="password" placeholder='Password'/>
+         </FormGroup>
+         <Button className="btn-lg  btn-dark">Log In </Button>
+         <div className='text-center pt-3'>
+           Or continue with your social account 
+         </div>
+        <FacebookLoginButton className='mt-3 mb-3'></FacebookLoginButton>
+         <div className='text-center pt-3'>
+           Or continue with your Google account 
+         </div>
+         <GoogleLoginButton className='mt-3 mb-3'/> 
+         <div className='text-center'>
+         <a href='/sign-up'>Sign Up</a>
+         <span className='p-2'>|</span>
+         <a href='/sign-up'>Forgot Password</a>
+         </div>
+    </Form> 
   );
 }
 
