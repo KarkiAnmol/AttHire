045f065fcb58c4c6a0072acbb27faf904a190809
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2022-11-04 08:40:30 +0545

    Password encrption feature added using bcryptjs module

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require("bcryptjs"); //requiring module for password encryption
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -41,5 +42,12 @@ const userSchema = new mongoose.Schema({
 
 }
 );
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")){ //checks if password is not modified, if found modified then it will be encrypted else it won't be encrypted again
+        next();
+    }
+
+    this.password= await bcrypt.hash(this.password,10);
+})
 
 module.exports =mongoose.model("User",userSchema);
