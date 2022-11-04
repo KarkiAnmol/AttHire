12368fa5c925a197fd45e3665f5c812d4be1a709
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2022-11-04 11:57:19 +0545

    user login created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ const User=require("../models/userModel");
 //Register a User
 exports.registerUser = catchAsyncError(async(req,res,next)=>{
     const{name,email,password} =req.body;
+
     const user= await User.create({
         name,email,password,
         avatar:{ //this portion will be updated later after completing frontend
@@ -12,8 +13,40 @@ exports.registerUser = catchAsyncError(async(req,res,next)=>{
             url:"profilePicture"
         }
     });
+    
+    const token = user.getJWTToken();
+    
     res.status(201).json({
         success:true,
-        user
+        token,
     });
+});
+
+//login user
+exports.loginUser = catchAsyncError(async(req,res,next)=>{
+    const {email,password} =req.body;
+
+    //checking if user has given email and password both
+    if(!email || !password){
+        return next(new ErrorHandler("Please enter both email and password!!!"),400)
+    }
+    
+    const user =await User.findOne({email}).select("+password"); //can't use password in findOne because it has been declared false previously while writing schema
+    
+    if(!user){
+    return next(new ErrorHandler("Invalid email or password"));
+    }
+
+    const isPasswordMatched =user.comparePassword(password); 
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+    const token = user.getJWTToken();
+    
+    res.status(200).json({
+        success:true,
+        token,
+    });
+    
 });
